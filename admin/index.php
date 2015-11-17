fa7896e47dbf0a726c5a2a741ebd79e875a574f4
vec <vec@moebaka.com>
2015-11-18 00:33:56 +0800

    后端外观修改

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,53 @@
 <?php
+require('../article.php');
 
-if ( !( isset($_GET['pw']) && $_GET['pw'] == 'asd123' ) ){
+if ( !( isset($_GET['pw']) && $_GET['pw'] == 'pache' ) ){
 	echo 'pw';
 	return 0;
+}
+if ( isset($_GET['page']) && (int)$_GET['page']>0 ){
+	if ( isset($_GET['display']) ){
+		$display = strtoupper($_GET['display']);
+	}
+	$page = (int) $_GET['page'];
 }else{
-	require('manageArticle.html');
+	$page = 1;
 }
 
 ?>
+<!doctype HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge;chrome=1"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<meta name="HandheldFriendly" content="true" />
+	<title>manageArticle</title>
+
+	<link href="style/admin.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="../js/remilia.js"></script>
+</head>
+<body>
+	<ul id="articlelist">
+		<?php
+
+		$list = getArticles(((int)$page - 1) * $pache->pagelimit, $pache->pagelimit);
+		for ( $i=0; $i<count($list); ++$i){
+			echo '<li><div class="link"><a href="get.php?id='. $list[$i]['id'] .'">'.$list[$i]['title'].'</a></div><div class="datetime">'.$list[$i]['time'].'</div></li>';
+		}
+		?>
+	</ul>
+	<div id="editor">
+		<form>
+			<input name="title" placeholder="标题" />
+			<textarea name="article" placeholder="正文"></textarea>
+			<select name="type">
+				<option value="markdown">Markdown</option>
+				<option value="text">text</option>
+			</select>
+			<button type="submit">post</button>
+		</form>
+	</div>
+</body>
+<script type="text/javascript" src="js/admin.js"></script>
+</html>
--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,4 +1,85 @@
+var articleIdList = document.getElementById('articlelist').getElementsByTagName('a');
+var mEvent = {
+	articleList: {
+		click: function (){
+			var id = $.getRequest(this.href, 'id');
+			manager.getArticleById( id,
+				function (){
+
+				},
+				function (){
+
+				}
+			);
+		}
+	}
+};
+var display = {
+	'editorForm': {},
+	animated: {
+		'Editor':{
+			'status': false,
+			'open': function (){
+				this.style.display = '';
+			},
+			'close': function (){
+				this.style.display = 'none';
+			},
+			'Toggle': function (){
+				console.info(this);
+				if ( this.style.display === undefined || this.style.display === '' ){
+					this.style.display = 'none';
+				}else{
+					this.style.display = '';
+				}
+			}
+		}
+	},
+	'constructForm': function (article){
+		var editor = $('#editor')[0];
+		var form = editor.getElementsByTagName('form');
+		this.form	= editor.getElementsByTagName('form')[0];
+		this.title	= document.getElementsByName('title')[0];
+		this.type = document.getElementsByName('type')[0];
+		this.article = document.getElementsByName('article')[0];
+
+		this.title.value = article.title;
+		this.article.value = article.article;
+
+
+		console.log('constructForm ok.');
+	},
+	loadEditor: function (article){
+		this.constructForm.apply( this.editorForm, [article] );
+		this.animated.Editor.open.apply( $('#editor')[0], [] );
+	}
+};
+display.animated.Editor.close.apply($('#editor')[0], []);
 
 var manager = {
-	getArticleList: function (){},
+	getArticleById: function (id, ok, fail){
+		var url = '../'+'get.php?'+ $.stringifyPostRequest({'id': id, 'display':'json'});
+		$.vjax( url, 'GET',
+			function (data){
+				try{
+					var article = JSON.parse(data);
+				}catch(e){
+					throw new Error(e);
+				}
+				display.loadEditor(article);
+			},
+			function (err){}
+		);
+	},
+	getArticleList: function (){
+
+	},
 };
+
+/* listing ArticleList links */
+for ( var i=0; i<articleIdList.length; ++i){
+	articleIdList[i].onclick = function (){
+		mEvent.articleList.click.apply(this, []);
+		return false;
+	}
+}
