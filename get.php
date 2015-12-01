df635cc5d62a5c5d6eeaa49405b39520e3f6ebd2
vec <vec@moebaka.com>
2015-12-01 19:48:00 +0800

    参数错误的时候也跳转到noFound页

--- get.php
+++ get.php
@@ -26,15 +26,16 @@ if ( isset($_GET['id']) ){
 	}
 
 }else{
-	echo '您的参数有误';
-	return 0;
+	$article = new stdClass;
+	$article->id = null;
 }
 
 if ( is_null($article->id) ){
+	$article = new stdClass;
 	$article->title = "出现了点问题";
-	$article->article = '<div id="float" style="position:relative;margin: auto;" ><img class="floatimg" src="articleNoFound.jpg" style="display:block; margin:auto;" /></div>当你看到这个页面的时候，说明文章因为不可抗力被删除了。或者你请求的id是一个未知数，Pache无法提供';
+	$article->article = '<div id="fail"><div id="float" style="position:relative;margin: auto;" ><img class="floatimg" src="articleNoFound.jpg" style="display:block; margin:auto;" /></div>当你看到这个页面的时候，说明文章因为不可抗力被删除了。或者你请求的id是一个未知数，Pache无法提供</div><div></div>';
 	$article->type = 'html';
-	$nofound = false;
+	$nofound = true;
 }
 
 ?>
@@ -71,7 +72,7 @@ if ( is_null($article->id) ){
 		<?php
 			echo "<h1>".$article->title."</h1>";
 		?>
-		<div id="time" <?php if ( isset($nofound) ){echo 'style="display:none;"';} ?> >
+		<div id="time" <?php echo isset($nofound) ? 'style="display:none;"' : ''; ?> >
 			创建时间：
 			<time>
 				<?php echo date("Y/m/d h:m:s", strtotime($article->time)); ?>
@@ -104,33 +105,33 @@ if ( is_null($article->id) ){
 
 ?>
 		</article>
-			tag: <?php
-				new outTagById($_GET['id'], 'html');
+			<?php
+				isset($nofound) || new outTagById($_GET['id'], 'html');
 			?>
 	</div>
 <?php
-
+if ( !isset($nofound) ){
 print <<<EOT
-<div id="duoshuo">
-<!-- 多说评论框 start -->
-	<div class="ds-thread" data-thread-key="$article->id" data-title="$article->title" data-url="http://$pache->root/get.php?id=$article->id"></div>
-<!-- 多说评论框 end -->
-<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
-<script type="text/javascript">
-var duoshuoQuery = {short_name:"vechk"};
-	(function() {
-		var ds = document.createElement('script');
-		ds.type = 'text/javascript';ds.async = true;
-		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-		ds.charset = 'UTF-8';
-		(document.getElementsByTagName('head')[0]
-		 || document.getElementsByTagName('body')[0]).appendChild(ds);
-	})();
-	</script>
-<!-- 多说公共JS代码 end -->
-</div>
+	<div id="duoshuo">
+	<!-- 多说评论框 start -->
+		<div class="ds-thread" data-thread-key="$article->id" data-title="$article->title" data-url="http://$pache->root/get.php?id=$article->id"></div>
+	<!-- 多说评论框 end -->
+	<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+	<script type="text/javascript">
+	var duoshuoQuery = {short_name:"vechk"};
+		(function() {
+			var ds = document.createElement('script');
+			ds.type = 'text/javascript';ds.async = true;
+			ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+			ds.charset = 'UTF-8';
+			(document.getElementsByTagName('head')[0]
+			 || document.getElementsByTagName('body')[0]).appendChild(ds);
+		})();
+		</script>
+	<!-- 多说公共JS代码 end -->
+	</div>
 EOT;
-
+}
  ?>
 </body>
 	<script type="text/javascript" src="pache.js"></script>
