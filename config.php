8f98ae4e4a1dbf36c993df938504faa8224fb895
vec <vec@moebaka.com>
2015-12-05 21:54:59 +0800

    以后配置信息都放这儿了

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/*
+	以后的配置信息都放这儿吧
+
+	诸如： admin密码，翻页等等……
+
+*/
+class pache{
+	public $admin = 'pache';
+	public $pagelimit = 10;
+	public $title = "myBlog";
+	public $root = "/pache";
+	public $updateAble = Array(
+		'title',
+		'article',
+		'type',
+		'class',
+	);
+	public $createAble = Array(
+		'title',
+		'article',
+		'type',
+		'class',
+	);
+}
+?>
