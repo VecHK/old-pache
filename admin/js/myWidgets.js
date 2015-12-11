de86ec181a64c0975faef657891f919ff7034763
vec <vec@moebaka.com>
2015-12-12 03:54:49 +0800

    tS 	变更为数组（ 	.clearAll
    	.createItem
    		支持 click 和 close 事件
    
    	.removeItem

--- admin/js/myWidgets.js
+++ admin/js/myWidgets.js
@@ -9,44 +9,61 @@ function tagSelector(ele){
 	this.appendItem = function (text){
 		return ele.appendChild(this.createItem(text));
 	};
-	this.createItem = function (text){
-		return $c('div', function (ele){
+	this.clearAll = function (f){
+		if ( this.length ){
+			f && f(this.pop());
+			arguments.callee(f);
+		}
+		ele.innerHTML = '';
+	};
+	this.createItem = function (conObj){
+		var my = this;
+		var tagSelectorEle = ele;
+		return $c('li', function (ele){
 			ele.append('div', function (contentEle){
 				contentEle[0].className = 'content';
-				if ( typeof text === 'string' ){
-					contentEle.text(text);
-				}else{
-					contentEle.append(text);
-				}
 				contentEle.css({
 					'position': 'relative'
 				});
+				function applyContent(content){
+					if ( typeof content === 'string' )
+						contentEle.text(content);
+					else
+						contentEle.append(content(contentEle));
+				}
+				if ( conObj instanceof window.Node || conObj instanceof window.HTMLElement ){
+					contentEle.append(conObj);
+				}else{
+					applyContent( typeof conObj !== 'object' ? conObj : conObj.content );
+				}
 			});
 			ele.css({
 				'position': 'relative'
 			});
 			ele.append($c('div', function (closeTag){
-				closeTag[0].className = 'closeTag';
 				closeTag.text('x');
-				closeTag.css({
-					'position': 'absolute',
-					'right': '0px',
-					top: '0px',
-					'cursor': 'pointer'
-				});
-			}))
+				closeTag.cssLine()
+					.position('absolute')
+					.right('0px')
+					.top('0px')
+					.cursor('pointer');
+				closeTag[0].className = 'closeTag';
+				this.addEvent('click', function (ele, cursor){
+					return function (){
+						if ( typeof conObj === 'object' && typeof conObj.close === 'function' && conObj.close.apply(tagSelectorEle, [ele]) ){
+							tagSelectorEle.removeChild(ele);
+							return my.removeItem(cursor);
+						}
+
+					}
+				}(ele[0], my.length), true);
+			}));
+			my.push(conObj[ conObj.value !== undefined ? 'value' : 'content' ]);
 		});
 	};
-	this.removeItem = function (){
-		el.removeChild(child)
+	this.removeItem = function (cursor){
+		return this.splice(cursor, cursor);
 	};
-	$(ele).css({
-		'position': 'absolute',
-		'height': '20px',
-		'width': '64px',
-		'top': '0px',
-		'right': '0px',
-		'color': '#fff'
-	});
 }
-var tS = new tagSelector( $('#tag-selector')[0] );
+var tS=[];
+tagSelector.apply( tS, [$('#tag .tag-selector')[0]] );
