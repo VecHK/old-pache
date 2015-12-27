3157ab6091c4ceecddd01147b88ab5dc12cf8ec9
vec <vec@moebaka.com>
2015-12-28 03:41:07 +0800

    articleList 开坑 	openArticle && closeArticle
    微调了 newadmin.js 的结构

--- admin/js/newadmin.js
+++ admin/js/newadmin.js
@@ -10,7 +10,7 @@ var model = new function (){
 		$.getJSON(url, conObj.ok, conObj.fail);
 	};
 	this.loadArticleById = function (conObj){
-		var url = '../get.php?' + $.stringify({
+		var url = '../get.php?' + $.stringifyRequest({
 			id: conObj.id,
 			display: 'json'
 		});
@@ -19,14 +19,108 @@ var model = new function (){
 };
 
 var viewer = new function (){
+	var viewer = this;
+	var articleListEle = $('#articlelist .list')[0];
+
+	var ResizeTextarea = function (a,row){
+		var agt = navigator.userAgent.toLowerCase();
+		var is_op = (agt.indexOf("opera") != -1);
+		var is_ie = (agt.indexOf("msie") != -1) && document.all && !is_op;
+		if(!a){return}
+		if(!row)
+			row=5;
+		var b=a.value.split("\n");
+		var c=is_ie?1:0;
+		c+=b.length;
+		var d=a.cols;
+		if(d<=20){d=40}
+		for(var e=0;e<b.length;e++){
+			if(b[e].length>=d){
+				c+=Math.ceil(b[e].length/d)
+			}
+		}
+		c=Math.max(c,row);
+		if(c!=a.rows){
+			a.rows=c;
+		}
+	};
+	this.eventResizeTextarea = function (){
+		ResizeTextarea(this, 4);
+	};
 	var renderingArticleList = function (articleList){
 		function row( articleInfo ){
-			console.info(articleInfo);
+			$(articleListEle).append('li', function(){
+				var classEle;
+
+				var titleEle = this.append('div', function (titleEle){
+					titleEle.className = 'title';
+
+					var titleName = this.append('h1', function (titleName){
+						titleName.className = 'title-name';
+
+						this.addEvent(
+							'click',
+							function (id){
+								return function (){
+									control.openArticle(id);
+								}
+							}( articleInfo.id ),
+							true
+						);
+
+						this.text(articleInfo.title);
+					})
+					var tagEle = this.append('ul', function (tagEle){
+						tagEle.className = 'tag';
+						var tagList = articleList.articlesTagList[ articleInfo.id ];
+
+						tagList.forEach(function ( item ){
+							var titleName = this.append('li', function (){
+								this.text(item);
+							});
+						}.bind(this));
+					});
+				});
+
+
+			});
 		};
 		articleList.articles.forEach(row);
 	};
 	this.showArticleList = function (articleList){
 		renderingArticleList(articleList);
+	};
+
+	var renderingEditor = function (article){
+		function setValueByName( name, value){
+			console.info($('#editor [name="'+ name +'"]')[0]);
+			$('#editor [name="'+ name +'"]')[0].value = value;
+		}
+		setValueByName( 'title', article.title );
+		setValueByName( 'content', article.article );
+		setValueByName( 'class', article.class);
+
+		function getEditorType(){
+			function getSelected( ele ){
+				return select.children[ select.selectedIndex ];
+			}
+			return getSelected( $('#editor .update select')[0] ).value;
+		}
+		function setEditorType( typeName ){
+			var select = $('#editor .update select')[0];
+			select.value = typeName;
+		}
+		setEditorType( article.type );
+
+		viewer.eventResizeTextarea.bind($('#editor .edit-area [name="content"]')[0])();
+	};
+	this.openEditor = function (article){
+		console.info(article);
+		$('#editor').fadeIn();
+		renderingEditor(article);
+	};
+	this.closeEditor = function (){
+		$('#editor').fadeOut();
 	};
 };
 
@@ -39,13 +133,8 @@ var control = new function (){
 		this.page = 0;
 		this.limit = 10;
 		this.updateType = null;
+		this.editorStatus = null;
 	};
-	this.page = 0;
-	this.limit = 10;
-
-	this.status = 0;
-
-	this.editorStatus = null;
 
 	this.showArticleList = function (){
 		model.loadArticleList({
@@ -58,15 +147,25 @@ var control = new function (){
 			}
 		});
 	};
+	this.openArticle = function (id){
+		model.loadArticleById({
+			'id': id,
+			ok: viewer.openEditor,
+			fail: function (err){
+				console.error(err);
+			}
+		})
+	};
 
 
 	var mEventF = (function (){
 		var load = function (){
-			//$('#editor').fadeOut();
+			$('#editor').fadeOut();
 			control.showArticleList();
 		};
 
 		window.onload = load;
+		$('#editor .close').addEvent('click', viewer.closeEditor, true)
 	}).bind(this);
 	var mEvent = new mEventF;
 
@@ -99,34 +198,10 @@ var control = new function (){
 		textarea自动变长
 		tanks http://www.aa25.cn/code/515.shtml
 	*/
-	function eventResizeTextarea(){
-		ResizeTextarea(this, 4);
-	}
-	function ResizeTextarea(a,row){
-		var agt = navigator.userAgent.toLowerCase();
-		var is_op = (agt.indexOf("opera") != -1);
-		var is_ie = (agt.indexOf("msie") != -1) && document.all && !is_op;
-		if(!a){return}
-		if(!row)
-			row=5;
-		var b=a.value.split("\n");
-		var c=is_ie?1:0;
-		c+=b.length;
-		var d=a.cols;
-		if(d<=20){d=40}
-		for(var e=0;e<b.length;e++){
-			if(b[e].length>=d){
-				c+=Math.ceil(b[e].length/d)
-			}
-		}
-		c=Math.max(c,row);
-		if(c!=a.rows){
-			a.rows=c;
-		}
-	}
+
 	$('#editor .edit-area [name="content"]')
-		.addEvent('click', eventResizeTextarea, true)
-		.addEvent('focus', eventResizeTextarea, true)
-		.addEvent('keydown', eventResizeTextarea, true);
+		.addEvent('click', viewer.eventResizeTextarea, true)
+		.addEvent('focus', viewer.eventResizeTextarea, true)
+		.addEvent('keydown', viewer.eventResizeTextarea, true);
 
 };
