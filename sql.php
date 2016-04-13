f9b090971ae0f6b6b4650f5a668481ea0c1a1b3a
vec <vec@moebaka.com>
2016-04-13 17:18:25 +0800

    之前没补充好的 hostker 服务器常量

--- sql.php
+++ sql.php
@@ -1,8 +1,8 @@
 <?php
 
 class sqlInfo{
-	public $database = "pache";
-	public $address = "localhost";	//MYSQL_DATABASE;
+	public $database = "pache";		//MYSQL_DATABASE
+	public $address = "localhost";
 	public $account = "root"; 		//MYSQL_USERNAME;
 	public $password = "root"; 		//MYSQL_PASSWORD;
 	public $con;
